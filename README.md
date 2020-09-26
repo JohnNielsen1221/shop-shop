564927795e077660e84eba3e28b990f4ed3bf49d
John Nielsen <johnhayesnielsen@gmail.com>
2020-09-26 11:42:58 -0600

    fixed detail page

---
+++ README.md
@@ -0,0 +1,51 @@
+# Shop-Shop
+>An eCommerce site that allows you to purchase goods (but not really).
+
+## Table of contents
+* [General info](#general-info)
+* [Installation](#installation)
+* [Functionality](#functionality)
+* [Screenshots](#screenshots)
+* [Status](#status)
+* [Link](#link)
+* [License](#license)
+* [Contact](#contact)
+
+## General info
+This program will allow you to add and remove items from your cart and checkout through Stripe.
+
+## Installation
+Steps required to install project and how to get the development environment running:
+
+- Clone the repo to your own system.
+- Run npm install from the command line in the root directory
+- Invoke the application with "npm start"
+
+This will connect you to the localhost:3001 where you can test the app.
+
+You may also test the app through the Heroku link listed in the links section.
+
+Additionally, since this is a PWA, supported browsers will allow you to install the program as an App. After loading the page in a browser, you should see a + inside of a circle on the right side of the address bar. Click on this and follow the prompts to install the program as a local App.
+
+## Functionality
+This is a basic eCommerce website that uses Stripe to collect your credit card information. You are able to view the items as a page, or view each individual item by clicking on them. You can then add them to your cart. You are also able to remove items from your cart by either clicking on the "Remove Items" button on an individual items page or clicking the trash icon in your cart. You can also manually change the item number in your cart.
+
+## Screenshots
+![Budget Home](budget-tracker-ss.png)
+
+## Status
+Project is: _complete_ (for now)
+
+## Links
+ - Respository Link - https://github.com/JohnNielsen1221/budget-tracker
+ - Heroku Link - https://calm-gorge-84926.herokuapp.com/
+
+## License
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+## Contact
+Created by John Hayes Nielsen - johnhayesnielsen@gmail.com
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,20 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
-import { useStoreContext } from "../utils/GlobalState";
+// import { useStoreContext } from "../utils/GlobalState";
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
   ADD_TO_CART,
   UPDATE_PRODUCTS,
-} from '../utils/actions';
+} from "../utils/actions";
 import { QUERY_PRODUCTS } from "../utils/queries";
-import spinner from '../assets/spinner.gif'
+import { idbPromise } from "../utils/helpers";
 import Cart from '../components/Cart';
-import { idbPromise } from '../utils/helpers';
+import spinner from '../assets/spinner.gif'
+import { useSelector, useDispatch } from 'react-redux';
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  // const [state, dispatch] = useStoreContext();
+  //use the redux useSelecter and useDispatch as the state and dispatch vairables
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({})
@@ -23,6 +27,33 @@ function Detail() {
 
   const { products, cart } = state;
 
+  useEffect(() => {
+    // already in global store
+    if (products.length) {
+      setCurrentProduct(products.find(product => product._id === id));
+      // retrieved from server
+    } else if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products
+      });
+
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+    }
+    // get cache from idb
+    else if (!loading) {
+      // since we're offline, get all of the data from the `products` store
+      idbPromise('products', 'get').then((indexedProducts) => {
+        // use retrieved data to set global state for offline browsing
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts
+        });
+      });
+    }
+  }, [products, data, loading, dispatch, id]);
 
   const addToCart = () => {
     const itemInCart = cart.find((cartItem) => cartItem._id === id)
@@ -53,37 +84,9 @@ function Detail() {
       type: REMOVE_FROM_CART,
       _id: currentProduct._id
     });
-
     // upon removal from cart, delete the item from IndexedDB using the `currentProduct._id` to locate what to remove
     idbPromise('cart', 'delete', { ...currentProduct });
   };
-
-  useEffect(() => {
-    // already in global store
-    if (products.length) {
-      setCurrentProduct(products.find(product => product._id === id));
-    }
-    // retrieved from server
-    else if (data) {
-      dispatch({
-        type: UPDATE_PRODUCTS,
-        products: data.products
-      });
-
-      data.products.forEach((product) => {
-        idbPromise('products', 'put', product);
-      });
-    }
-    // get cache from idb
-    else if (!loading) {
-      idbPromise('products', 'get').then((indexedProducts) => {
-        dispatch({
-          type: UPDATE_PRODUCTS,
-          products: indexedProducts
-        });
-      });
-    }
-  }, [products, data, loading, dispatch, id]);
 
   return (
     <>
@@ -108,8 +111,7 @@ function Detail() {
             </button>
             <button
               disabled={!cart.find(p => p._id === currentProduct._id)}
-              onClick={removeFromCart}
-            >
+              onClick={removeFromCart}>
               Remove from Cart
             </button>
           </p>
